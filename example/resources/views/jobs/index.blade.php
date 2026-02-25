cce8c9e930cf42bc6bc138b0e88d8b389d69a59a
DaeStan <stanifed@my.erau.edu>
2026-02-25 12:54:17 -0700

    Completed Example Project

--- example/resources/views/jobs/index.blade.php
+++ example/resources/views/jobs/index.blade.php
@@ -7,13 +7,13 @@
          @foreach ($jobs as $job)
             <div class="space-y-4 text-lg">
                 <a href="/jobs/{{ $job['id'] }}" class="block px-4 py-6 border border-gray-200 rounded-lg">
-                    <div class="font-bold text-[#2b4155] text-sm">
+                    <div class="font-bold text-blueone text-sm">
                         {{$job->employer->name}}
                     </div>
                     <div>
                         <strong>{{$job['title']}}:</strong> Pays {{$job['salary']}} per year.
                     </div> 
-                    <div class="flex item-center gap-1 font-bold text-[#446888] text-sm">
+                    <div class="flex item-center gap-1 font-bold text-bluetwo text-sm">
                         <img src="https://www.svgrepo.com/show/486434/location-filled.svg" class="size-5"/>
                         {{$job->location->name}}
                     </div>                         
